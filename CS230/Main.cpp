aa8ade24a5d509ed19f08c0361c130c78f7d2e76
Taeju Kwon <taeju.kwon@digipen.edu>
2021-05-14 06:25:57 +0900

    Week_10

--- CS230/Main.cpp
+++ CS230/Main.cpp
@@ -19,7 +19,7 @@ Creation date: 2/10/2021
 int main(void) {
 	try {
 		Engine& engine = Engine::Instance();
-		engine.Init("Week 9");
+		engine.Init("Week 10");
 		Splash splash;
 		MainMenu mainmenu;
 		Level1 level1;
